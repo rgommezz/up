98468b25ea86ba05f3ceaeb72231a8018dc22d30
Raúl Gómez Acuña <4982414+rgommezz@users.noreply.github.com>
2020-01-28 16:44:42 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 #
 # This script should be run via:
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/juliantellez/up/master/install.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/rgommezz/up/master/install.sh)"
 #
 GITHUB_SHA=${1:-master}
 
 echo "\n UP Github SHA: ${GITHUB_SHA} \n"
 
 mkdir -p ~/.up; cd ~/.up; \
-curl -L "https://github.com/juliantellez/up/tarball/${GITHUB_SHA}" | tar -xzv --strip-components 1 &> /dev/null; \
+curl -L "https://github.com/rgommezz/up/tarball/${GITHUB_SHA}" | tar -xzv --strip-components 1 &> /dev/null; \
 sh bootstrap.sh
